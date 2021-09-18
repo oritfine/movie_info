8f8d4b5be63c9bcef1365efa2b45c8f663e21e7e
OritFine <orityf545@gmail.com>
2021-09-18 20:08:00 +0300

    init commit

---
+++ lib/route_generator.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+import 'movie_page.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/moviePage':
+        if (args is String) {
+          return MaterialPageRoute(
+            builder: (_) => MoviePage(
+              data: args,
+            ),
+          );
+        }
+        return errorRoute();
+      default:
+        return errorRoute();
+    }
+  }
+
+  static Route<dynamic> errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Error"),
+        ),
+        body: Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
